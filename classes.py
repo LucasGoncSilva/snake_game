99ef7413dc1debc2b429f4dab6ac440a7c980d02
Lucas Gonçalves da Silva <lucasgoncsilva04@gmail.com>
2022-03-11 16:24:07 -0300

    game done

--- classes.py
+++ classes.py
@@ -1,6 +1,6 @@
 from random import randint
 
-
+import pygame
 from pygame import Surface
 from pygame.mixer import music
 from pygame.locals import *
@@ -16,7 +16,6 @@ class Area:
         self.top = (WIN_SIZE[1] - self.height - self.left) // PX_SIZE * PX_SIZE
         self.right = self.left + self.width
         self.bottom = self.top + self.height
-        print(self.left, WIN_SIZE[0]-self.right)
 
 
 class LimitedArea(Area):
@@ -55,7 +54,6 @@ class Snake(Area):
         self.direction = K_LEFT
 
         self.walking = 1
-        self.collided = 0
 
     def head(self) -> tuple:
         return self.pos[0]
@@ -102,20 +100,49 @@ class Snake(Area):
             self.walking = 0
             music.load('sounds/lose.wav')
             music.play(0)
+    
+    def regenerate(self) -> None:
+        x = (self.right + self.left) // 2 // PX_SIZE * PX_SIZE
+        y = (self.bottom + self.top) // 2 // PX_SIZE * PX_SIZE
+
+        self.pos = [
+            (x, y),
+            (x + PX_SIZE, y),
+            (x + PX_SIZE * 2, y)
+        ]
+
+        self.skin1 = Surface((PX_SIZE, PX_SIZE))
+        self.skin1.fill(SECONDARY_COLOR)
+
+        self.skin2 = Surface((PX_SIZE, PX_SIZE))
+        self.skin2.fill(ACCENT_COLOR1)
+
+        self.direction = K_LEFT
+
+        self.walking = 1
 
 
 class Apple(Area):
     def __init__(self) -> None:
         Area.__init__(self)
 
-        x = randint(self.left, self.right)
-        y = randint(self.top, self.bottom)
+        x = randint(self.left + PX_SIZE, self.right - 2 * PX_SIZE)
+        y = randint(self.top + PX_SIZE, self.bottom - 2 * PX_SIZE)
         self.pos = (x // PX_SIZE * PX_SIZE, y // PX_SIZE * PX_SIZE)
 
         self.surface = Surface((PX_SIZE, PX_SIZE))
         self.surface.fill(RED)
 
     def regenerate(self) -> None:
-        x = randint(self.left, self.right)
-        y = randint(self.top, self.bottom)
+        x = randint(self.left + PX_SIZE, self.right - 2 * PX_SIZE)
+        y = randint(self.top + PX_SIZE, self.bottom - 2 * PX_SIZE)
         self.pos = (x // PX_SIZE * PX_SIZE, y // PX_SIZE * PX_SIZE)
+
+
+class Score:
+    def __init__(self) -> None:
+        self.value = 0
+    
+    def add(self) -> None: self.value += 1
+
+    def reset(self) -> None: self.value = 0
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+from datetime import datetime
+
 import pygame as pg
 from pygame.locals import *
+from pygame_menu import Theme, Menu, events
 
 from constants import *
-from classes import Snake, Apple, LimitedArea
+from classes import Snake, Apple, LimitedArea, Score
 from functions import *
 
 
 pg.init()
-
 
 win = pg.display.set_mode(WIN_SIZE)
 pg.display.set_caption('Snake Game')
@@ -16,59 +18,114 @@ pg.display.set_caption('Snake Game')
 snake = Snake()
 apple = Apple()
 area = LimitedArea()
+score = Score()
 
 
-while 1:
-    # loop constants
-    pg.time.Clock().tick(15)
-    win.fill(MAIN_COLOR)
+def restart():
+    snake.regenerate()
+    apple.regenerate()
+    score.reset()
+    run()
 
 
-    # event detection
-    for event in pg.event.get():
-        if event.type == QUIT: exit_game()
-
-        if event.type == KEYDOWN:
-            if event.key == K_q or event.key == K_ESCAPE: exit_game()
-
-            elif event.key == K_UP and snake.direction != K_DOWN:
-                snake.go_to(K_UP)
-
-            elif event.key == K_DOWN and snake.direction != K_UP:
-                snake.go_to(K_DOWN)
-
-            elif event.key == K_RIGHT and snake.direction != K_LEFT:
-                snake.go_to(K_RIGHT)
-
-            elif event.key == K_LEFT and snake.direction != K_RIGHT:
-                snake.go_to(K_LEFT)
+def run():
+    while 1:
+        # loop constants
+        pg.time.Clock().tick(15)
+        win.fill(MAIN_COLOR)
+        font = pg.font.Font(r'.\fonts\PixeloidSans-nR3g1.ttf', 30)
 
 
-    # collisions detection
-    if collision(snake.head(), apple.pos):
-        snake.grow()
-        apple.regenerate()
+        # event detection
+        for event in pg.event.get():
+            if event.type == QUIT: exit_game()
 
-        for pos in snake.body():
-            if collision(pos, apple.pos):
-                apple.regenerate()
-    
-    if area.off_limits(snake.head()):
-        snake.stop()
+            if event.type == KEYDOWN:
+                if event.key == K_q or event.key == K_ESCAPE: exit_game()
+
+                elif event.key == K_UP and snake.direction != K_DOWN:
+                    snake.go_to(K_UP)
+
+                elif event.key == K_DOWN and snake.direction != K_UP:
+                    snake.go_to(K_DOWN)
+
+                elif event.key == K_RIGHT and snake.direction != K_LEFT:
+                    snake.go_to(K_RIGHT)
+
+                elif event.key == K_LEFT and snake.direction != K_RIGHT:
+                    snake.go_to(K_LEFT)
+                
+                elif event.key == K_r and not snake.walking:
+                    restart()
 
 
-    # draw
-    snake.walk()
+        # collisions detection
+        if collision(snake.head(), apple.pos):
+            score.add()
+            snake.grow()
+            apple.regenerate()
 
-    for i, pos in enumerate(snake.full_body()):
-        if i % 2 != 1:
-            win.blit(snake.skin1, pos)
+            for pos in snake.body():
+                if collision(pos, apple.pos):
+                    apple.regenerate()
+        
+        if area.off_limits(snake.head()):
+            snake.stop()
+        
+
+        # draw
+        snake.walk()
+
+        for i, pos in enumerate(snake.full_body()):
+            if i % 2 != 1:
+                win.blit(snake.skin1, pos)
+            else:
+                win.blit(snake.skin2, pos)
+        
+        win.blit(apple.surface, apple.pos)
+
+        pg.draw.rect(win, area.color, pg.Rect(area.left, area.top, area.width, area.height), area.border)
+
+        if snake.walking:
+            score_text = font.render(f'Score: {score.value}', True, ACCENT_COLOR2)
+            score_text_rect = score_text.get_rect()
+            score_text_rect.left = area.left
+            score_text_rect.top = area.left
+            win.blit(score_text, score_text_rect)
+
         else:
-            win.blit(snake.skin2, pos)
-    
-    win.blit(apple.surface, apple.pos)
+            if datetime.now().second % 2 == 0:
+                lose_text = font.render(f'Score: {score.value} points! Press "r" to restart!', True, RED)
+            else:
+                lose_text = font.render(f'Score: {score.value} points! Press "r" to restart!', True, ACCENT_COLOR2)
 
-    pg.draw.rect(win, area.color, pg.Rect(area.left, area.top, area.width, area.height), area.border)
-    # pg.draw.rect(win, area.color, pg.Rect(45, 40, 600, 585), area.border)
-    
-    pg.display.update()
+            lose_text_rect = lose_text.get_rect()
+            lose_text_rect.center = (WIN_SIZE[0] / 2, (area.top) / 2)
+            win.blit(lose_text, lose_text_rect)
+
+        pg.display.update()
+
+
+theme = Theme(
+    # general
+    selection_color = ACCENT_COLOR2,
+    # title
+    title = True,
+    title_font = r'.\fonts\PixeloidSans-nR3g1.ttf',
+    title_font_color = MAIN_COLOR,
+    title_background_color = ACCENT_COLOR2,
+    # background
+    background_color = MAIN_COLOR,
+    # widget
+    widget_font = r'.\fonts\PixeloidSans-nR3g1.ttf',
+    widget_font_color = ACCENT_COLOR1
+)
+
+menu = Menu('Snake Game', WIN_SIZE[0], WIN_SIZE[1], theme=theme)
+
+menu.add.button('Play', run)
+menu.add.button('Quit', events.EXIT)
+menu.add.vertical_margin(20, 'm')
+menu.add.label('Arrow Keys to move:\n◄▲▼►')
+
+menu.mainloop(win)
